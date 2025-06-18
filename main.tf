4212139d4b01701be9672792fd044a4c5d241e6b
aishwaryravi <aishwary.ravi9@gmail.com>
2025-06-18 11:27:40 +0000

    Second commit

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = var.aws_access_key # Only for testing, use env vars for production
-  secret_key = var.aws_secret_key  # Only for testing
 }
 module "ecr" {
   source       = "./modules/ecr"
